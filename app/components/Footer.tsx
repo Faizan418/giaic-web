4fd1e67159c58b275fa8c49cf0257183991f1d61
Muhammad Faizan Soomro <141871174+Faizan418@users.noreply.github.com>
2024-10-19 11:55:55 +0500

    done

---
+++ app/components/Footer.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div>
+        <p className='bg-slate-400 justify-center flex'>&copy;2024; created by faizan</p>
+    </div>
+  )
+}
+
---
+++ app/components/Header.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function Header() {
+  return (
+    <div>
+          <nav className="nav-bar">
+        <div className="logo w-20 h-full"> 
+          <Image src="/images/logo.png" alt="logo" width={80} height={10} />
+        </div>
+
+        <h2 className="navbar-h2">Tuition Free Education Proram on Latest Technologies</h2>
+
+        <ul className="ul-list gap-5">
+         <Link href="/"><li>Home</li></Link>
+         <Link href="/"><li>Apply</li></Link>
+         <Link href="/"><li>Jobs</li></Link>
+         <Link href="/"><li>Result</li></Link>
+         <Link href="/"><li>Courses</li></Link>
+
+        </ul>
+
+      </nav>
+    </div>
+  )
+}
+
